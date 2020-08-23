77eaf4139e6930d00a5943a46c0c436be2a2e670
609529897 <609529897@qq.com>
2020-08-23 12:26:14 +0800

    feat: add buildURL function

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Method = 'get' | 'GET'
+  | 'delete' | 'Delete'
+  | 'head' | 'HEAD'
+  | 'post' | 'POST'
+  | 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
