9df0340acbb4ec616e4e437e6575add0827ced09
609529897 <609529897@qq.com>
2020-08-23 15:35:46 +0800

    feat:add a function to handle request_headers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,19 @@
-export type Method = 'get' | 'GET'
-  | 'delete' | 'Delete'
-  | 'head' | 'HEAD'
-  | 'post' | 'POST'
-  | 'patch' | 'PATCH'
+export type Method =
+  'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
   url: string
   method?: Method
   data?: any
   params?: any
+  headers?: any
 }
